831176763e63759735bbce41ab241428e11826e6
torippi <61878057+torippi@users.noreply.github.com>
2023-02-02 08:55:34 +0900

    テーブルの作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-sample';
+
+  musicData = [
+    {
+      No: '0000-0001',
+      Name: '私だけが好きだった。',
+      Artist: 'すいそうぐらし',
+      Youtube: 'https://www.youtube.com/watch?v=WnVDf7Wkg4Y'
+    }
+  ]
 }
