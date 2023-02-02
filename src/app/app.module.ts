63ce0eb25949bbcb6d7a60c34042fdcd5da89272
torippi <61878057+torippi@users.noreply.github.com>
2023-02-03 05:47:57 +0900

    Youtubeの埋め込みとIframeをコンポーネントとして活用

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IframeComponent } from './iframe.component';
+
+import {RouterModule, Route} from '@angular/router'
+
+const ROUTES: Route[] = [
+  {path: '', redirectTo:'welcome', pathMatch: 'full'},
+  {path: 'welcome',component: AppComponent}, 
+  {path: 'classic', children: [
+    {path: '**', component: IframeComponent}
+  ]}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IframeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [],
   bootstrap: [AppComponent]
