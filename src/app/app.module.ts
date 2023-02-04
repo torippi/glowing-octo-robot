22a614d162f31f913a7f50d10366ad9e1b158837
torippi <61878057+torippi@users.noreply.github.com>
2023-02-04 20:14:48 +0900

    Angularのアップデートのため更新

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { YoutubePlayerExampleModule } from './youtube-module';
 
 @NgModule({
+  // このモジュールに属するコンポーネント
   declarations: [
     AppComponent
   ],
+  // このモジュールで利用する他のモジュール
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    YoutubePlayerExampleModule
   ],
+  // サービスクラスを登録。APIとかで利用。
   providers: [],
+  // アプリで最初に起動すべき最上位のコンポーネント（＝ルートコンポーネント）
   bootstrap: [AppComponent]
 })
 export class AppModule { }
