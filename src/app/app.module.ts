757ba0c89aab9cf52a303490e78c6ba4dbcf7bef
torippi <61878057+torippi@users.noreply.github.com>
2023-02-04 22:06:00 +0900

    Revert "Revert "Revert "Angular アップグレード"""
    This reverts commit d201b35ec01bd4a3b6fc489d1993df63c004bbff.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IframeComponent } from './iframe.component';
-
-import { RouterModule, Route } from '@angular/router'
-
-const ROUTES: Route[] = [
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: 'welcome', component: AppComponent },
-  {
-    path: 'classic', children: [
-      { path: '**', component: IframeComponent }
-    ]
-  }
-]
 
 @NgModule({
-  // このモジュールに属するコンポーネント
   declarations: [
-    AppComponent,
-    IframeComponent
+    AppComponent
   ],
-  // このモジュールで利用する他のモジュール
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(ROUTES),
+    AppRoutingModule
   ],
-  // サービスクラスを登録。APIとかで利用。
   providers: [],
-  // アプリで最初に起動すべき最上位のコンポーネント（＝ルートコンポーネント）
   bootstrap: [AppComponent]
 })
 export class AppModule { }
