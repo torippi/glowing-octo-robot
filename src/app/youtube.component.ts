5ded5729b366d5122e30fa8c1fa93175f12424dd
torippi <61878057+torippi@users.noreply.github.com>
2023-02-20 01:02:32 +0900

    最初の動画のランダム

--- src/app/youtube.component.ts
+++ src/app/youtube.component.ts
@@ -20,12 +20,14 @@ let apiLoaded = false;
 })
 
 export class YoutubePlayerComponent {
+    VideoIds = ['WnVDf7Wkg4Y', 'JHq6kYWdgNo'];
     VideoId = 'WnVDf7Wkg4Y';
     VideoState = 0;
     VideoPlayer: any;
 
     // Component Start 
     ngOnInit() {
+        this.VideoId = this.VideoIds[this.RandomInt(this.VideoIds.length)];
         if (!apiLoaded) {
             // This code loads the IFrame Player API code asynchronously, according to the instructions at
             // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
@@ -50,7 +52,7 @@ export class YoutubePlayerComponent {
         // https://developers.google.com/youtube/iframe_api_reference?hl=ja#:~:text=player.getPlayerState()%3ANumber
         this.VideoState = event.target.getPlayerState();
         if (this.VideoState == 0) {
-            this.VideoId = 'JHq6kYWdgNo';
+            this.VideoId = this.VideoId[this.RandomInt(this.VideoIds.length)];
         }
         else if (this.VideoState == 5) {
             event.target.playVideo();
@@ -63,5 +65,9 @@ export class YoutubePlayerComponent {
 
     onStopClick(event: any){
         this.VideoPlayer.pauseVideo();
+    }
+
+    RandomInt(ram: any){
+        return Math.floor( Math.random() * ram );
     }
 }
